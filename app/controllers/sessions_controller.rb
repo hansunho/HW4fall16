be93976845fcb4f0a9ce46c84d92756a8a2fa3bb
hansunho <han.sunho92@gmail.com>
2016-09-30 21:10:16 +0000

    all done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-  if  (User.exists?(user_id: params[:user][:user_id], email: params[:user][:email])) then
-    @user = User.find_by(user_id: params[:user][:user_id], email: params[:user][:email] )
-    session[:session_token] = @user.session_token 
-     flash[:notice] = "You are logged in as #{params[:user][:user_id]}"
-      redirect_to movies_path
-  else
-     flash[:notice] = "Invalid user-id/e-mail combination"
-      redirect_to login_path
+    if  (User.exists?(user_id: params[:user][:user_id], email: params[:user][:email])) then
+      @user = User.find_by(user_id: params[:user][:user_id], email: params[:user][:email] )
+      session[:session_token] = @user.session_token 
+       flash[:notice] = "You are logged in as #{params[:user][:user_id]}"
+        redirect_to movies_path
+    else
+       flash[:notice] = "Invalid user-id/e-mail combination"
+        redirect_to login_path
+    end
   end
-end
 
   def destroy
     session[:session_token]=nil
